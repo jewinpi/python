0430e0c7b513e2e3e8bb96c6485ac26c5fb25e1a
jewinpi <48846185+jewinpi@users.noreply.github.com>
2019-03-29 10:06:24 +1100

    Update fibonacci_sequence.py

--- fibonacci_sequence.py
+++ fibonacci_sequence.py
@@ -3,10 +3,13 @@ i = 0
 i1 = 1
 if endNum > 0:
   print('0')
+  print('1')
   while endNum >= i:
-    print(i1)
     i = i + i1
-    print(i)
     i1 = i + i1 
+    if endNum >= i: 
+      print(i)
+    if endNum >= i1:
+      print(i1)
 else:
   print('Cannot have a range equal or less than 0')
