cca2ad562bafe5186cadea6500d5ca9b516ece4e
jewinpi <48846185+jewinpi@users.noreply.github.com>
2019-03-29 09:49:48 +1100

    Update and rename roman_numeral.py to fibonacci_sequence.py

---
+++ fibonacci_sequence.py
@@ -0,0 +1,12 @@
+endNum = int(input('Fibonacci sequence range: ')) 
+i = 0
+i1 = 1
+if endNum > 0:
+  print('0')
+  while endNum >= i:
+    print(i1)
+    i = i + i1
+    print(i)
+    i1 = i + i1 
+else:
+  print('Cannot have a range equal to 0')
