2919b905d3b31209dbbc0182cd2610d46567d6a4
jewinpi <48846185+jewinpi@users.noreply.github.com>
2019-04-03 22:04:25 +1100

    Update leap_years.py

--- leap_years.py
+++ leap_years.py
@@ -1,11 +1,14 @@
-leapTest = int(input('Find if a year is a leap year: ')) #gets user input for 
-if leapTest%4 == 0:
-  print(leapTest , 'is a leap year')
-else:
-  print(leapTest , 'is not a leap year')
-leapYears = [2020, 2021]
-for x in leapYears:
-  if x%4 == 0:
-    print(x, 'is a leap year aswell')
-  else:
+leapTest = int(input('Find if a year is a leap year: ')) #gets user input to test for leap year
+if leapTest > 0: # simple test to see if the integar inputted is positive
+ if leapTest%4 == 0: # tests of inputted value can be divided by 4 evenly
+   print(leapTest , 'is a leap year') 
+ else: # if the value inputted cannot be divided by 4 evenly
+   print(leapTest , 'is not a leap year')
+else: # else statement for if the value entered was a negative
+  print('Cannot have a negative year!')
+leapYears = [2020, 2021] # list of a leap and non leap year to test method
+for x in leapYears: # loops through each value in the list
+  if x%4 == 0: # checks if a value in the list can be divided by 4 evenly
+    print(x, 'is a leap year')
+  else: # else statement for is a value in the list is not divisible by 4 evenly
     print(x, 'is not a leap year')
