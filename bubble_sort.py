1578c61551c6efd589d1905e46ac70bed39a85bb
jewinpi <48846185+jewinpi@users.noreply.github.com>
2019-04-25 12:29:12 +1000

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,23 +1,25 @@
-def swap_fun():
+def swadef swap_fun():
   x = nums.pop(i1)
   nums.insert(i, x)
   print(nums)
+  y = 0
 nums = [6,9,8,1,2,4]
 print(nums)
 i = 0
 i1 = 1
 y = 0
-while True: 
-  while i1 <= len(nums):
-    try:
-     if nums[i] > nums[i1]:
-       swap_fun()
-       i += 1
-       i1 += 1
-     elif nums[i] <= nums[i1]:
-       i += 1
-       i1 += 1
-    except:
-      i = 0
-      i1 = 1
-  break
+while i1 <= len(nums):
+  try:
+    if nums[i] > nums[i1]:
+      swap_fun()
+      i += 1
+      i1 += 1
+    if y >= len(nums):
+      break
+    elif nums[i] <= nums[i1]:
+      i += 1
+      i1 += 1
+      y += 1
+  except:
+    i = 0
+    i1 = 1
